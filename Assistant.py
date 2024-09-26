17ca184ab427456c910aa732325455ff42c06c02
codeash <codeashwin03@gmail.com>
2024-09-26 20:24:32 +0530

    partially completed with small changes

--- Assistant.py
+++ Assistant.py
@@ -58,27 +58,6 @@ def username():
     print("Welcome",uname.center(columns))
     speak("how can i help you")    
 
-# def takeCommand():
-
-#     r = sr.Recognizer()
-
-#     with sr.Microphone() as source:
-#         print("Listening...")
-#         r.pause_threshold = 1
-#         audio = r.listen(source)
-
-#         try:
-#             print("Recognizing...")
-#             query = r.recognize_google(audio, language='en-in')
-#             print(f"User said: {query}\n")
-
-#         except Exception as e:
-#             print(e)
-#             print("Unable to Recognize Your Voice")
-#             return "None"
-    
-#     return query
-
 def takeCommand():
     r = sr.Recognizer()
 
